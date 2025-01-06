2733a6b09911a3161344b103a8f9962fd72c0d91
EgorovNikolay <egorovnikolay14021994@gmail.com>
2025-01-06 04:57:43 +0300

    fixed decorators.py and test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -2,7 +2,9 @@ def log(filename=None):
     def wrapper(func):
         def inner(*args, **kwargs):
             try:
-                print(f"Начало выполнения функции {func.__name__} с аргументами {args} и {kwargs}")
+                log_message = f"Начало выполнения функции {func.__name__} с аргументами {args} и {kwargs}\n"
+                if filename is None:
+                    print(log_message)
                 for arg in args:
                     if not isinstance(arg, (int, float)):
                         raise ValueError("Аргументы должны быть целым или вещественным числом")
@@ -10,26 +12,33 @@ def log(filename=None):
                     if not isinstance(value, (int, float)):
                         raise ValueError("Аргументы должны быть целым или вещественным числом")
                 result = func(*args, **kwargs)
-                print(f"{func.__name__} ok")
-                print(f"Результат работы функции {result}")
-                print(f"Конец выполнения функции {func.__name__} с аргументами {args} и {kwargs}")
-                if filename:
+                result_message = f"Результат работы функции {result}\n"
+                end_message = f"Конец выполнения функции {func.__name__} с аргументами {args} и {kwargs}\n"
+                success_message = f"{func.__name__} ok\n"
+
+                if filename is None:
+                    print(result_message)
+                    print(end_message)
+                    print(success_message)
+                else:
                     with open(filename, "a") as f:
-                        f.write(f"{func.__name__} ok\n")
+                        f.write(log_message)
+                        f.write(result_message)
+                        f.write(end_message)
+                        f.write(success_message)
                 return result
             except Exception as e:
                 error_message = f"Функция: {func.__name__} Ошибка: {str(e)}. Введенные аргументы: {args}, {kwargs}\n"
-                print(error_message)
                 if filename:
                     with open(filename, "a") as file:
-                        file.write(error_message + "\n")
-
+                        file.write(error_message)
+                else:
+                    print(error_message)
         return inner
-
     return wrapper
 
 
-@log(filename="mylog.txt")
+@log()
 def my_function(x, y):
     """Функция складывает два числа"""
     return x + y
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,46 +1,57 @@
+import pytest
 from src.decorators import my_function
 
 
-def test_successful_execution(capsys):
+def test_console_logging_success(capsys):
     result = my_function(2, 2)
+    captured = capsys.readouterr()
     assert result == 4
-
-    captured = capsys.readouterr()
-    assert (
-        "Начало выполнения функции my_function с аргументами (2, 2) и {}"
-        in captured.out
-    )
-    assert "my_function ok" in captured.out
+    assert "Начало выполнения функции my_function с аргументами (2, 2) и {}" in captured.out
     assert "Результат работы функции 4" in captured.out
-    assert (
-        "Конец выполнения функции my_function с аргументами (2, 2) и {}" in captured.out
-    )
+    assert "Конец выполнения функции my_function с аргументами (2, 2) и {}" in captured.out
+    assert "my_function ok" in captured.out
 
 
-def test_invalid_argument(capsys):
-    result = my_function("2", 2)
-    assert result is None
-
+def test_console_invalid_value(capsys):
+    result = my_function('2', 2)
     captured = capsys.readouterr()
-    assert (
-        "Начало выполнения функции my_function с аргументами ('2', 2) и {}"
-        in captured.out
-    )
-    assert (
-        "Функция: my_function Ошибка: Аргументы должны быть целым или вещественным числом. "
-        "Введенные аргументы: ('2', 2), {}"
-        in captured.out
-    )
+    assert result is None
+    assert "Начало выполнения функции my_function с аргументами ('2', 2) и {}" in captured.out
+    assert ("Функция: my_function Ошибка: Аргументы должны быть целым или вещественным числом. "
+            "Введенные аргументы: ('2', 2), {}") in captured.out
 
 
-def test_empty_argument(capsys):
+def test_console_empty_value(capsys):
     result = my_function()
-    assert result is None
-
     captured = capsys.readouterr()
+    assert result is None
     assert "Начало выполнения функции my_function с аргументами () и {}" in captured.out
-    assert (
-        "Функция: my_function Ошибка: my_function() missing 2 required positional arguments: 'x' and 'y'. "
-        "Введенные аргументы: (), {}"
-        in captured.out
-    )
+    assert ("Функция: my_function Ошибка: my_function() missing 2 required positional arguments: 'x' and 'y'. "
+            "Введенные аргументы: (), {}") in captured.out
+
+
+def test_console_success_kwargs(capsys):
+    result = my_function(4, y=3)
+    captured = capsys.readouterr()
+    assert result == 7
+    assert "Начало выполнения функции my_function с аргументами (4,) и {'y': 3}" in captured.out
+    assert "Результат работы функции 7" in captured.out
+    assert "Конец выполнения функции my_function с аргументами (4,) и {'y': 3}" in captured.out
+    assert "my_function ok" in captured.out
+
+
+def test_console_invalid_value_kwargs(capsys):
+    result = my_function(4, y='3')
+    captured = capsys.readouterr()
+    assert result is None
+    assert "Начало выполнения функции my_function с аргументами (4,) и {'y': '3'}" in captured.out
+    assert "Функция: my_function Ошибка: Аргументы должны быть целым или вещественным числом. Введенные аргументы: (4,), {'y': '3'}" in captured.out
+
+
+def test_console_empty_kwargs(capsys):
+    result = my_function(y='3')
+    captured = capsys.readouterr()
+    assert result is None
+    assert "Начало выполнения функции my_function с аргументами () и {'y': '3'}" in captured.out
+    assert "Функция: my_function Ошибка: Аргументы должны быть целым или вещественным числом. Введенные аргументы: (), {'y': '3'}" in captured.out
+
