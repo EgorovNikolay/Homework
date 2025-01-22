d758d17a3f59e34a5abcd1b8501468974364c16c
EgorovNikolay <egorovnikolay14021994@gmail.com>
2025-01-23 01:34:10 +0300

    done homework 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,55 @@
+import logging
+import os
+
+base_path = os.path.dirname(__file__)
+full_path = os.path.join(base_path, "..", "logs", "masks.log")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler_file = logging.FileHandler(full_path, "w")
+formater_file = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+handler_file.setFormatter(formater_file)
+logger.addHandler(handler_file)
+
+
 def get_mask_card_number(number_card: str) -> str:
     """Функция маскирует номер карты"""
-    if len(number_card) != 16:
-        raise ValueError("Номер карты должен содержать 16 цифр")
-    if not number_card.isdigit():
-        raise ValueError("Номер карты должен содержать только цифры")
-    return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
+    logger.info(f"Начало маскировки номера карты: {number_card}")
+    try:
+        if len(number_card) != 16:
+            logger.error(f"Ошибка длины номера карты: {number_card}")
+            raise ValueError("Номер карты должен содержать 16 цифр")
+        if not number_card.isdigit():
+            logger.error(f"Номер карты содержит не только цифры: {number_card}")
+            raise ValueError("Номер карты должен содержать только цифры")
+        logger.info("Маскировка номера карты завершена")
+        return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
+    except ValueError:
+        logger.error("Ошибка маскировки номера карты")
+        return ''
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскирует номер счета"""
-    if len(account_number) != 20:
-        raise ValueError("Номер счета должен содержать 20 цифр")
-    if not account_number.isdigit():
-        raise ValueError("Номер счета должен содержать только цифры")
-    return f"**{account_number[-4:]}"
+    logger.info(f"Начало маскировки номера счета: {account_number}")
+    try:
+        if len(account_number) != 20:
+            logger.error(f"Ошибка длины номера счета: {account_number}")
+            raise ValueError("Номер счета должен содержать 20 цифр")
+        if not account_number.isdigit():
+            logger.error(f"Номер счета содержит не только цифры: {account_number}")
+            raise ValueError("Номер счета должен содержать только цифры")
+        logger.info("Маскировка номера счета завершена")
+        return f"**{account_number[-4:]}"
+    except ValueError:
+        logger.error("Ошибка маскировки номера счета")
+        return ''
+
+
+correct_number_card = get_mask_card_number("1234123412341234")
+invalid_number_card = get_mask_card_number("abcdef")
+
+correct_number_account = get_mask_account("01234567890123456789")
+invalid_number_account = get_mask_account("abcdef")
--- src/utils.py
+++ src/utils.py
@@ -4,33 +4,35 @@ import os
 from typing import Dict, List
 
 base_path = os.path.dirname(__file__)
-full_path = os.path.join(base_path, '..', 'logs', 'utils.log')
+full_path = os.path.join(base_path, "..", "logs", "utils.log")
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(full_path, 'w')
-file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler(full_path, "w")
+file_formater = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
 
 
 def read_json_file(path_to_file: str) -> List[Dict]:
-    """Функция читает json файл по указанному пути и возвращает список словарей """
-    logger.info(f'Чтение файла по указанному пути {path_to_file}')
+    """Функция читает json файл по указанному пути и возвращает список словарей"""
+    logger.info(f"Чтение файла по указанному пути {path_to_file}")
     try:
-        with open(path_to_file, 'r') as file:
+        with open(path_to_file, "r") as file:
             data = json.load(file)
             if isinstance(data, list):
-                logger.info('Файл возвращен в виде списка')
+                logger.info("Файл возвращен в виде списка")
                 return data
-            logger.warning('Файл не является списком')
+            logger.warning("Файл не является списком")
             return []
     except (FileNotFoundError, json.JSONDecodeError):
-        logger.error(f'Путь {path_to_file} не найден')
+        logger.error(f"Путь {path_to_file} не найден")
         return []
 
 
-test_data_invalid = read_json_file('wrong_path')
-file_path = os.path.join('..', 'data', 'operations.json')
+test_data_invalid = read_json_file("wrong_path")
+file_path = os.path.join("..", "data", "operations.json")
 test_data_correct = read_json_file(file_path)
